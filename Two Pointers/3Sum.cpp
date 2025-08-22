80aaa170ef8dc862c61d02020f9344c591d3d20e
NITISH1-1 <9891nr@gmail.com>
2025-08-22 20:09:35 +0530

    added tow pointer approach

--- Two Pointers/3Sum.cpp
+++ Two Pointers/3Sum.cpp
@@ -1,3 +1,35 @@
+// LeetCode Problem: 15. 3Sum
+//Using Two Pointers Technique
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> st;
+        for(int i = 0;i<nums.size(); i++) {
+            set<int> hashset;
+            for(int j = i+1; j<nums.size(); j++){
+                int third = -(nums[i] + nums[j]);
+                if(hashset.find(third) != hashset.end()){
+                    vector<int> temp = {nums[i], nums[j], third};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(nums[j]);
+            }
+        }
+
+        vector<vector<int>> result(st.begin(), st.end());
+        return result;
+    }
+};
+
+
+
+
+
+//Best Approach
+
+
 #include <vector>
 class Solution {
 public:
